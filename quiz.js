d708799a57ffc75999b38aaf992bbb03bf6b2bb3
MirzaAyon <ayonmirza98@gmail.com>
2022-06-17 17:53:03 +0600

    pin mather added

---
+++ quiz.js
@@ -0,0 +1,29 @@
+// quiz - 
+// function generatePin() {
+//     return Math.floor(Math.random() * 90000) + 10000;
+// }
+
+
+// quiz - 9
+// const product = 5; 
+// const price = "7"; 
+// const subTotal = product * price; 
+// const tax = subTotal/10; 
+// const total = subTotal + tax; 
+
+
+//quiz 11
+// We know that <span> tag is an inline element. Which is true for the following code?(Let's recap HTML)
+
+// <span><h1>Heading is a block-level element</h1><span></span>
+
+// ans :  to wrap a block element we need another block level element
+
+//quiz 14
+//What is an anonymous function?
+//A func without name
+
+//quiz 15
+// What does Math.random() return?
+//A float point
+//0.6075
